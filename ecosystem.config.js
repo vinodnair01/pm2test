231725d7563640a03c866423545530a27d8d4d0e
Vinod <Vinod@mytest.com>
2018-10-03 06:07:48 -0400

    Test files

---
+++ ecosystem.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  apps : [{
+    name: 'Test',
+    script: 'app.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: 'one two',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'wayfinder',
+      host : '18.236.233.194',
+      ref  : 'origin/master',
+      repo : 'https://github.com/vinodnair01/pm2test.git',
+      path : '/home/wayfinder/public_html',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
